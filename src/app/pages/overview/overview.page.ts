3532828f38b43c2933c1a0f012400b122f314fe6
Trend20 <enockomondi305@gmail.com>
2022-10-13 15:51:20 +0300

    working on the overview page

--- src/app/pages/overview/overview.page.ts
+++ src/app/pages/overview/overview.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-overview',
@@ -10,6 +10,5 @@ export class OverviewPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
-
+    }
 }
